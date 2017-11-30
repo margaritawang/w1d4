944abe296d8fc41dff51b6013cff06fcc3eef33f
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-11-30 19:51:16 +0000

    replaced for with forEach

---
+++ find_waldo.js
@@ -0,0 +1,28 @@
+/*function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + '!');
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound); */
+
+function findWaldo(arr, found) {
+  arr.forEach (function(item,index) {
+    if ( item === "Waldo") {
+      // console.log("Found Waldo at index " + index + '!');   // execute callback
+    found(index);
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + '!');
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
