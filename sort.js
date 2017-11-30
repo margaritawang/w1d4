5f9b8995c1e11ef1fb784b0915f6b1b43e83b58c
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-11-30 20:30:54 +0000

    sorted people by name and age

---
+++ sort.js
@@ -0,0 +1,27 @@
+var array = [10, 2, 5, 1, 9];
+var newArray = array.sort(function(a,b) {
+  return a - b;
+});
+
+console.log(newArray);
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var newList = students.sort(function(a,b) {
+  if (a['name'] < b['name']) {
+    return -1;
+  }
+  else if (a['name'] === b['name']) {
+    return a['age'] - b['age'];
+  }
+  else {
+    return 1;
+  }
+});
+
+console.log(newList);
